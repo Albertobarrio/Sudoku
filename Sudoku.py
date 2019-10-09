c64fce8e2a7296442cfcf0ec662e9aab112172a8
Albertobarrio <albertobarrio2012@gmail.com>
2019-10-08 22:53:47 -0300

    first commit

---
+++ Sudoku.py
@@ -0,0 +1,49 @@
+class InvalidPosition(Exception):
+    pass
+
+
+class InvalidColumn(Exception):
+    pass
+
+
+class InvalidRow(Exception):
+    pass
+
+
+class Sudoku():
+
+    def __init__(self, board):
+        self.original_board = list(board)
+        self.position_fixed = []
+        self.is_position_fixed(self.original_board)
+        
+    def is_position_fixed(self, original_board):
+        for position, number in enumerate(self.original_board):
+            if number != 'x':
+                self.position_fixed.append(position)
+    
+    def number_in_column(self, lineal_position):
+        column = []
+        lineal_position = lineal_position % 9
+        while lineal_position <= 80:
+            column.append(self.original_board[lineal_position])
+            lineal_position += 9
+        return column
+
+    def number_in_row(self, lineal_position):
+        column = []
+        lineal_position = (lineal_position // 9)*9
+        for iterator in range(0, 9):
+            column.append(self.original_board[lineal_position])
+            lineal_position += 1
+        return column
+    
+    def put_number(self, position_x, position_y, number):
+        lineal_position = (position_x * 9) + position_y
+        if lineal_position in self.position_fixed:
+            raise InvalidPosition()
+        elif str(number) in self.number_in_column(lineal_position):
+            raise InvalidColumn()
+        elif str(number) in self.number_in_row(lineal_position):
+            raise InvalidRow
+    
---
+++ test_sudoku.py
@@ -0,0 +1,74 @@
+import unittest
+
+from Sudoku import (
+    Sudoku,
+    InvalidPosition,
+    InvalidColumn,
+    InvalidRow,
+)
+
+
+class TestSudoku(unittest.TestCase):
+
+    def test_put_number_1(self):
+        sudoku = Sudoku("53xx7xxxx6xx195xxxx98xxxx6x8xxx6xxx34xx8x3xx17xxx2xxx6x6xxxx28xxxx419xx5xxxx8xx79")
+        with self.assertRaises(InvalidPosition):
+            sudoku.put_number(0, 0, 6)  # x,y,numero
+    
+    def test_put_number_2(self):
+        sudoku = Sudoku( "53xx7xxxx6xx195xxxx98xxxx6x8xxx6xxx34xx8x3xx17xxx2xxx6x6xxxx28xxxx419xx5xxxx8xx79")
+        with self.assertRaises(InvalidColumn):
+            sudoku.put_number(2, 0, 5)  
+    
+   
+    def test_put_number_3(self):
+        sudoku = Sudoku( "53xx7xxxx6xx195xxxx98xxxx6x8xxx6xxx34xx8x3xx17xxx2xxx6x6xxxx28xxxx419xx5xxxx8xx79")
+        with self.assertRaises(InvalidRow):
+            sudoku.put_number(0, 3, 7)  
+    
+    
+    
+    """
+    def test_game_not_over(self):
+        sudoku = Sudoku( "53xx7xxxx 6xx195xxx x98xxxx6x 8xxx6xxx3 4xx8x3xx1 7xxx2xxx6 x6xxxx28x xxx419xx5 xxxx8xx79")
+        over = sudoku.is_over()
+        self.assertFalse(over)
+    
+   
+    
+    
+    
+    def test_put_number_4(self):
+        sudoku = Sudoku( "53xx7xxxx 6xx195xxx x98xxxx6x 8xxx6xxx3 4xx8x3xx1 7xxx2xxx6 x6xxxx28x xxx419xx5 xxxx8xx79")
+        self.assertEqual(sudoku.put_number(3,9,1),"Numero incorrecto,repetido en la region")#x,y,numero
+    
+    def test_put_number_5(self):
+        sudoku = Sudoku( "53xx7xxxx 6xx195xxx x98xxxx6x 8xxx6xxx3 4xx8x3xx1 7xxx2xxx6 x6xxxx28x xxx419xx5 xxxx8xx79")
+        self.assertEqual(sudoku.put_number(3,9,1),"El numero se pudo colocar")#x,y,numero
+
+    def test_no_win(self):
+        sudoku = Sudoku( "53xx7xxxx 6xx195xxx x98xxxx6x 8xxx6xxx3 4xx8x3xx1 7xxx2xxx6 x6xxxx28x xxx419xx5 xxxx8xx79")
+        over = sudoku.is_over()
+        self.assertFalse(over)
+    
+    def test_no_win_after_play(self):
+        sudoku = Sudoku( "53xx7xxxx 6xx195xxx x98xxxx6x 8xxx6xxx3 4xx8x3xx1 7xxx2xxx6 x6xxxx28x xxx419xx5 xxxx8xx79")
+        sudoku.put_number(3,9,1)
+        over = sudoku.is_over()
+        self.assertFalse(over)
+
+    def test_win(self):
+        sudoku = Sudoku( "tablero lleno")
+        over = sudoku.is_over()
+        self.assertTrue(over)
+
+    def test_win_after_last_play(self):
+        sudoku = Sudoku( "tablero casi lleno")
+        sudoku.put_number(3,9,1)
+        over = sudoku.is_over()
+        self.assertTrue(over)
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
