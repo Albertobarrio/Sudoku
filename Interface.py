fcba25a97fd7a599bd54b3cdcb1406ae0844e83e
Albertobarrio <albertobarrio2012@gmail.com>
2019-10-26 20:10:07 -0300

    Sudoku v.1

--- Interface.py
+++ Interface.py
@@ -9,33 +9,31 @@ from Sudoku import (
 class IncorrectNumber(Exception):
     pass
 
-class NotNumber(Exception):
-    pass
 
 class Interface():
     def __init__(self):
         self.sudoku = Sudoku("534678912672195348198342567859761423426853791713924856961537284287419635345286x79")
     
-    def validate_input_number(self, number):
+    def validate_input_number(self, number, position_x, position_y):
         
-        if int(number) >= 10 or int(number) <= 0:
-            raise IncorrectNumber()
-        elif not int: 
-            raise NotNumber()
-        else:
+        if number.isalpha() or position_x.isalpha() or position_y.isalpha(): 
+            raise ValueError()
+        if 1 <= int(number) <= 9 and -1 < int(position_x) < 9 and -1 < int(position_y) < 9  :
             return True
+        raise IncorrectNumber()
 
     def play_sudoku(self):
         print(self.sudoku.print_board())
         try:
             while not self.sudoku.is_over():
                 self.number = input("Ingrese un numero: ")
-                self.position_x = int(input("Ingrese la posicion x: "))
-                self.position_y = int(input("Ingrese la posicion y: "))
-                if self.validate_input_number(self.number):
-                    self.sudoku.put_number(self.position_x,self.position_y,self.number)
+                self.position_x = input("Ingrese la posicion x: ")
+                self.position_y = input("Ingrese la posicion y: ")
+                if self.validate_input_number(self.number,self.position_x,self.position_y):
+                    self.sudoku.put_number(int(self.position_x),int(self.position_y),int(self.number))
                 print(self.sudoku.print_board())
-        
+            print("Felicitaciones ganaste")
+
         except InvalidPosition:
             print("No se puedo colocar el numero, hay un numero fijo")
             self.play_sudoku()
@@ -55,11 +53,14 @@ class Interface():
         except IncorrectNumber:
             print("Ha ingresado incorrecto, debe ingresar un numero entre 1 y 9")
             self.play_sudoku()
+
+        # except TypeError:
+            # print("Ha ingresado una letra")
+            # self.play_sudoku()     
         
-        except NotNumber:
+        except ValueError:
             print("Ha ingresado una letra")
             self.play_sudoku()
-            
 
 Interface().play_sudoku()
             
--- test_interface.py
+++ test_interface.py
@@ -3,9 +3,11 @@ import unittest
 from Interface import ( 
     Interface,
     IncorrectNumber,
-    NotNumber,
 )
 
+# FIXME: Arreglar las recursividades 
+# TODO: Validar las coordenadas
+# TODO: Api
 
 class TestSudoku(unittest.TestCase):
 
@@ -13,21 +15,37 @@ class TestSudoku(unittest.TestCase):
        self.interface = Interface()
 
     def test_valid_number(self):
-        self.assertTrue(self.interface.validate_input_number(5))
+        self.assertTrue(self.interface.validate_input_number(1,8,6))
     
     def test_input_small_number(self):
         with self.assertRaises(IncorrectNumber):
-            self.interface.validate_input_number(0)
-    """
+            self.interface.validate_input_number(0,8,6)
+    
     def test_input_big_number(self):
         with self.assertRaises(IncorrectNumber):
-            self.interface.validate_input_number(11)
+            self.interface.validate_input_number(11,8,6)
+
+
+    def test_input_big_number_position_x(self):
+        with self.assertRaises(IncorrectNumber):
+            self.interface.validate_input_number(1,11,6)
+
+
+    def test_input_small_number_position_x(self):
+        with self.assertRaises(IncorrectNumber):
+            self.interface.validate_input_number(1,-1,6)
+
+    def test_input_big_number_position_y(self):
+        with self.assertRaises(IncorrectNumber):
+            self.interface.validate_input_number(1,8,11)
+
+    def test_input_small_number_position_y(self):
+        with self.assertRaises(IncorrectNumber):
+            self.interface.validate_input_number(1,8,11)
 
     def test_input_letter(self):
-        with self.assertRaises(NotNumber):
-            self.interface.validate_input_number("l")
-    """
-
+        with self.assertRaises(TypeError):
+            self.interface.validate_input_number("l",8,6)
 
 if __name__ == '__main__':
     unittest.main()
